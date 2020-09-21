0cf88b4d5985e26ff1629513c980a65234aa72c9
astatochek <71458498+astatochek@users.noreply.github.com>
2020-09-21 21:50:57 +0300

    Add files via upload

---
+++ cubes.cpp
@@ -0,0 +1,15 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+// вводится количество чисел, затем эти числа
+// выводится среднее арифметическое кубов введенных чисел
+int main() {
+    int n, sum = 0, count = 0, value;
+    cin >> n;
+    while(n--){
+        cin >> value;
+        sum += value*value*value;
+        count++;
+    }
+    cout << float(sum)/float(count) << endl;
+}
