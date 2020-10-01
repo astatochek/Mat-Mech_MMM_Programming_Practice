0e77b4439406ff67d2829d63726f0e12f33ec90b
astatochek <71458498+astatochek@users.noreply.github.com>
2020-10-01 17:07:02 +0300

    Update Rational.cpp

--- Rational.cpp
+++ Rational.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 using namespace std;
-// структура - рациональные числа
+
+// СТРУКТУРА - РАЦИОНАЛЬНЫЕ ЧИСЛА
 
 // вычисление НОД
 int gcd(int a, int b){
     if (b==0) return a;
-    return gcd(b, a%b);
+    return abs(gcd(b, abs(a)%b));
 };
 
 // вычисление НОК
 int lcd(int a, int b){
-    return (a * b) / gcd(a, b);
+    return abs((a * b) / gcd(a, b));
 };
 
 struct Rational {
@@ -34,8 +35,15 @@ struct Rational {
     };
 
     Rational &operator /= (Rational &x){
-        nom *= x.denom;
-        denom *= x.nom;
+
+        if (x.nom < 0){
+            denom += -x.nom;
+            nom *= -x.denom;
+        }
+        else{
+            nom *= x.denom;
+            denom *= x.nom;
+        }
         int current_gcd = gcd(nom, denom);
         nom /= current_gcd;
         denom /= current_gcd;
@@ -46,7 +54,10 @@ struct Rational {
         int current_lcd = lcd(denom, x.denom);
         nom *= current_lcd / denom;
         denom = current_lcd;
-        nom += x.nom * current_lcd / x.denom;
+        if (x.nom < 0)
+            nom += -1 * int(abs(x.nom) * current_lcd / x.denom);
+        else
+            nom += x.nom * current_lcd / x.denom;
         int current_gcd = gcd(nom, denom);
         nom /= current_gcd;
         denom /= current_gcd;
@@ -57,7 +68,30 @@ struct Rational {
         int current_lcd = lcd(denom, x.denom);
         nom *= current_lcd / denom;
         denom = current_lcd;
-        nom -= x.nom * current_lcd / x.denom;
+        if (x.nom < 0)
+            nom -= -1 * int(abs(x.nom) * current_lcd / x.denom);
+        else
+            nom -= x.nom * current_lcd / x.denom;
+        int current_gcd = gcd(nom, denom);
+        nom /= current_gcd;
+        denom /= current_gcd;
+        return *this;
+    };
+
+    Rational &operator *= (int &x){
+        nom *= x;
+        int current_gcd = gcd(nom, denom);
+        nom /= current_gcd;
+        denom /= current_gcd;
+        return *this;
+    };
+
+    Rational &operator /= (int &x){
+        if (x < 0){
+            nom *= -1;
+            denom *= abs(x);
+        }
+        else denom *= x;
         int current_gcd = gcd(nom, denom);
         nom /= current_gcd;
         denom /= current_gcd;
@@ -71,6 +105,14 @@ Rational &operator * (Rational x, Rational &y){
     return x *= y;
 };
 
+Rational &operator * (Rational x, int &y){
+    return x *= y;
+};
+
+Rational &operator / (Rational x, int &y){
+    return x /= y;
+};
+
 Rational &operator / (Rational x, Rational &y){
     return x /= y;
 };
@@ -82,22 +124,22 @@ Rational &operator + (Rational x, Rational &y){
 Rational &operator - (Rational x, Rational &y){
     return x -= y;
 };
-ostream& operator<< (ostream &out, Rational &x){
+ostream &operator << (ostream &out, Rational &x){
     if (x.denom == 0) out << "Inf";
     else if (x.nom == 0) out << 0;
+    else if (x.denom == 1) out << x.nom;
     else out << x.nom << "/" << x.denom;
     return out;
 };
 
 int main() {
-// код еще сырой, если делить ан ноль, то не возникнет ошибки.
-// Так ноль выводится тольо если у дроби числитель равен нулю.
-// Бесконечность выводится, если у дроби знаменатель равен нулю
-    Rational a(17, 4), b(25, 8);
-    Rational c = a*b, d = a/b, e = a+b, f = a-b;
+    Rational a(17, 4), b(-34, 8);
+    int g = 10, h = -10;
+    Rational c = a*b, d = a/b, e = a+b, f = a-b, i = a*g, j = a/h;
     cout << a << " * " << b << " = " << c << endl;
     cout << a << " : " << b << " = " << d << endl;
     cout << a << " + " << b << " = " << e << endl;
     cout << a << " - " << b << " = " << f << endl;
-
+    cout << a << " * " << g << " = " << i << endl;
+    cout << a << " : " << h << " = " << j << endl;
 }
